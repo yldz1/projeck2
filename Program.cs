338b72a701ca1d46d796612270fbe6ed4c7558ae
Yıldız Nur İbrahim <mehmetyildiznur8@gmail.com>
2024-11-01 23:37:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milisaniyeden_Saniyeye
+{
+    class Program
+    {
+        //Environment sınıfının TickCount elemanını bir değişkene atayın
+        //ms cinsinden olan bu değeri dakika olarak ekrana yazdırın
+        //not: küsüratlı olabilsin
+        static void Main(string[] args)
+        {
+            var tms = Environment.TickCount;
+            var ts = tms / 1000;
+            var dk = ts / 60;
+            var s = dk / 60;
+            Console.WriteLine(s + " saat");
+            Console.ReadLine();
+
+        }
+    }
+}
